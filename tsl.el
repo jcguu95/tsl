20b3df5bed2001b193b3470d549581900f072a18
jcguu95 <jcguu95@gmail.com>
2021-03-25 08:27:49 -0500

    extend opener, now it parses control string

--- tsl.el
+++ tsl.el
@@ -45,29 +45,67 @@ example,
                          :store #'org-tsl-store-link)
 
 (defun org-tsl-open (str arg)
-  ;; TODO arg is universal arg? learn how to use. TODO more
-  ;; options to be added. TODO image => sxiv? TODO images =>
-  ;; dired? sxiv loop? TODO audio(s)/video(s) => emm? Use dired
-  ;; by default.. really open the files if universal argument is
-  ;; provided. in the later case, if there are multiple files,
-  ;; ask the user by using ivy what to do next: either ivy,
-  ;; force-open, or select one (or multiple?) to force-open.
+  ;; TODO arg is universal arg? learn how to use.
   ;;
-  ;; TODO allow link format
-  ;; [[tsl:20191026::((hash . "3e4d") (name . "screen"))]]
+  ;; TODO more
+  ;; options to be added.
+  ;;
+  ;; TODO image => sxiv? images => dired? sxiv loop?
+  ;; audio(s)/video(s) => emm?
+  ;;
+  ;; Use dired by default.. really open the files if universal
+  ;; argument is provided. in the later case, if there are
+  ;; multiple files, ask the user by using ivy what to do next:
+  ;; either ivy, force-open, or select one (or multiple?) to
+  ;; force-open.
+  ;;
+  ;; DONE- allow link format
+  ;; [[tsl:20191026::(:name "screen" :hash "^3e4d")]]
+  ;; [[tsl:20191026::(:name "screen" :hash "3e4d")]]
+  ;; [[tsl:20191026::(:hash "3e4d" :name "screen")]]
+  ;; [[tsl:20191026::(:hash "3e4d" :name ("screen" "jpeg"))]]
   ;;
   ;; TODO 自動偵測 filetype 並給予建議
 
   (let* ((query (nth 0 (split-string str "::")))
          (result (tsl:find query))
-         (len (length result))
 
          (lisp (nth 1 (split-string str "::")))
          (lst (when lisp (read lisp))))
 
-    (if lst
-        (with-info lst (dired result))  ;; TODO to implement! (DSL)
-      (dired result))))
+    (cl-labels ((md5sum (file)
+                        (with-demoted-errors "Error: %S"
+                          (and (file-exists-p file)
+                               (with-temp-buffer
+                                 (insert-file-contents file)
+                                 (md5 (buffer-string))))))
+                (filter-with (result lst)
+                             ;; main function to filter using lisp control string.
+                             (let ((hash (plist-get lst :hash))
+                                   (name (plist-get lst :name)))
+                               (when hash
+                                 (progn
+                                   (message "Computing hash might take a while.")
+                                   (setf result
+                                         (-filter (lambda (file)
+                                                    (string-match hash (md5sum file)))
+                                                  result))))
+                               (when name
+                                 (when (atom name) (setf name (list name)))
+                                 (loop for n in name
+                                       do (setf result
+                                                (-filter (lambda (file)
+                                                           (string-match n (f-full file)))
+                                                         result))))
+                               result)))
+      ;; Filter the results with lisp control string.
+      (setf result (filter-with result lst))
+      (if result
+          (progn
+            (dired (cons "" result))
+            (message "%s match(es) found for query -- %s. Full result: %s"
+                     (length result) str result))
+        (message "No matches! Sorry!")))))
 
 (defun org-tsl-store-link ()
   "Store a link to a timestamped link."
