01e18eda8432e7abb3bf4dcc9b56b5af041f4048
jcguu95 <jcguu95@gmail.com>
2021-04-14 10:17:14 -0500

    add tsl:find-at-point

--- tsl.el
+++ tsl.el
@@ -69,7 +69,6 @@ example,
 ;; TODO add a time profiler.
 
 
-
 ;;; ;;; ;;; ;;; ;;; ;;; ;;; ;;; ;;;
 ;;; org link of customized type ;;;
 ;;; ;;; ;;; ;;; ;;; ;;; ;;; ;;; ;;;
@@ -159,10 +158,12 @@ example,
   ;; FIXME This interferes with org-capture even without the context of TSL.
   ;; org-capture calls org-store-link, which calls org-store-link-functions..
   ;; so I shouldn't use read-from-minibuffer.
-  (let* ((ts (read-from-minibuffer "Time: "))
-         (lisp (read-from-minibuffer "Lisp: "))
-         (link (concat "tsl" ":" ts "::" lisp))
-         (description (read-from-minibuffer "Description: ")))
+  (let* (;; (ts (read-from-minibuffer "Time: "))
+         ;; (lisp (read-from-minibuffer "Lisp: "))
+         ;; (link (concat "tsl" ":" ts "::" lisp))
+         ;; (description (read-from-minibuffer "Description: "))
+         ts lisp link description
+         )
     (org-link-store-props
      :type "tsl"
      :link link
@@ -178,4 +179,92 @@ example,
     tsl:*search-history*
     nil nil #'string=)))
 
+
+;; experiment
+;;
+;;
+(defun tsl:check-ts-format (str)
+  "Check if the input string is an expected time string.
+
+Mechanism: first check if it's of length 4, 6, 8, 11, 13, or 15.
+If not, return nil. If yes, complete the string canonically so
+that it's of length 15.
+
+Next, break them into tokens, and check if they are as expected."
+  ;; if length 4, can be any number
+  ;; if length 6, the last two should be 01~12
+  ;; if length 8, after translating to a date, should be a legit date
+  ;; if length 11, first 8 should give a legit date, 9th should be a -, 10+11 should be between 00~23
+  ;; if length 13, ..etc + the last two should be 00~59
+  ;; if length 15, ..etc + the last two should be 00~59
+  ;;
+  ;; ;; test cases
+  ;; (mapcar #'tsp:check-ts-format '("2020" "20210731-150156")) ;; all t
+  ;; (mapcar #'tsp:check-ts-format '("20200731-" "20210731-150161")) ;; all nil
+  (let ((len (length str)))
+    (when
+        ;; First sanity check.
+        (cl-case len
+          (4 (setf str (concat str "0101-000000")))
+          (6 (setf str (concat str "01-000000")))
+          (8 (setf str (concat str "-000000")))
+          (11 (setf str (concat str "0000")))
+          (13 (setf str (concat str "00")))
+          (15 str))
+      ;; Break them into tokens, and check if as expected.
+      (ignore-errors
+        (let ((year (read (substring str 0 4)))
+              (month (read (substring str 4 6)))
+              (day (read (substring str 6 8)))
+              (dash (read (substring str 8 9)))
+              (hour (read (substring str 9 11)))
+              (minute (read (substring str 11 13)))
+              (sec (read (substring str 13 15))))
+          (and (and (integerp year) (> year 0))
+               (and (integerp month) (<= 1 month 12))
+               (and (integerp day) (<= 1 day 31))
+               (eq '- dash)
+               (and (integerp hour) (<= 0 hour 23))
+               (and (integerp minute) (<= 0 minute 59))
+               (and (integerp sec) (<= 0 sec 59))))))))
+(defun tsl:extract-ts-from-string (str)
+  "The core utility that extracts time stamps from any given
+string."
+  ;; ;; test cases
+  ;; (mapcar #'tsp:extract-ts-from-string
+  ;;         (list
+  ;;          "This is a long--message- asd -s--20210107 -12--sd ok"
+  ;;          "This is a long--message- asd -s--20210107-12--sd ok"
+  ;;          "This is a long--message- asd -s--20210131-085932--sd ok"
+  ;;          "This is a long--message- asd -s--20210170--sd ok"
+  ;;          "This is a long--message- asd -s--202101--sd ok"))
+  (let ((result (-uniq
+                 (-filter (lambda (x) (tsl:check-ts-format x))
+                          (-flatten
+                           (s-match-strings-all
+
+                            (rx word-start
+                                (** 4 15 (any digit "-"))
+                                word-end)
+
+                            str)
+
+                           )))))
+
+    ;; This is just a weird hack to prevent "legal" timestrings
+    ;; like "101010" .. and it would still have some edge cases.
+    (-filter (lambda (x) (>= (length x) 8)) result)
+
+    ))
+
+(defun tsl:find-at-point ()
+  ;; Only support "full" time string at this moment.
+  (interactive)
+  (let* ((str (thing-at-point 'sentence 'no-property))
+         (ts (tsl:extract-ts-from-string str)))
+    (dired-other-window (-flatten (-uniq (mapcar #'tsl:find ts))))))
+
+;;
+;;
+;;
 (provide 'tsl)
